777dcce7fef742bc550896bcb5ed51297141b195
Anna Nowak <moneyladder911@gmail.com>
2026-04-24 00:28:39 +0400

    Change gallery-2 span to row-span-2 for vertical portrait visibility

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -6,7 +6,7 @@ const GalleryPage: React.FC = () => {
     { span: 'md:col-span-2 md:row-span-2', title: 'Island Inn Tennis Club', url: '/gallery/island-inn-sign.jpg', color: '#68a1c9', pos: 'center' },
     { span: 'md:row-span-2', title: 'Kim and Players', url: '/gallery/IMG_6521.jpg', color: '#2a5a3a', pos: 'center 20%' },
     { span: 'md:row-span-2', title: 'Seasonal Mixer at Island Inn', url: '/gallery/gallery-1.jpg', color: '#1a3a2a', pos: 'center 20%' },
-    { span: 'md:col-span-2', title: 'Summer Family Tennis', url: '/gallery/gallery-2.jpg', color: '#c96b3a', pos: 'top' },
+    { span: 'md:row-span-2', title: 'Summer Family Tennis', url: '/gallery/gallery-2.jpg', color: '#c96b3a', pos: 'center' },
     { span: 'lg:col-span-3 aspect-[3/1] md:aspect-auto', title: 'Tennis Community under the Oaks', url: '/gallery/gallery-5.jpg', color: '#1e5c38', pos: 'center 40%' },
     { span: '', title: 'Private Instruction Support', url: '/gallery/gallery-3.jpg', color: '#b89a5e', pos: 'center 15%' },
     { span: 'md:row-span-2', title: 'Nomans Garden View', url: '/gallery/nomans-garden.jpg', color: '#153d20', pos: 'center' },
