5546727321f8841d9ef1dfb25c5edd4b4073549a
Anna Nowak <moneyladder911@gmail.com>
2026-07-02 02:25:06 +0400

    Newsletter signup: redirect to /thank-you via serverless fn

---
+++ api/newsletter.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
+
+  try {
+    const body = req.body || {};
+    const response = await fetch('https://formspree.io/f/xjgjjekd', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({ ...body, _type: 'newsletter' }),
+    });
+
+    if (response.ok) {
+      res.redirect(302, '/thank-you');
+    } else {
+      res.redirect(302, '/');
+    }
+  } catch {
+    res.redirect(302, '/');
+  }
+}
